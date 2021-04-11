5881022fed478ac154714579ca290d50e7de7580
Sandro095 <80029296+Sandro095@users.noreply.github.com>
2021-04-11 22:56:56 +0200

    ispravak

--- Vjezbe4/calculus.py
+++ Vjezbe4/calculus.py
@@ -1,5 +1,7 @@
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def f1(x):
     return x**3-5*x
@@ -7,16 +9,12 @@ def f1(x):
 def fd(x):
     return 3*x**2-5
 
-a=[]
-b=[]
-r=[]
-f=[]
-#3x**2-2
-
 def value(func,x):
     return(func(x))
 
 def ana_funk(func,xd,xg,e):
+    r=[]
+    f=[]
     d=int((xg-xd)/e)
     for i in(range(d)):
         p=xd+i*e
@@ -25,11 +23,17 @@ def ana_funk(func,xd,xg,e):
         f.append(k)
     return r,f
 
-def deriviraj(funk,xo,e):
-    der =  (value(f1,xo+e)-value(f1,xo-e))/(2*e)
-    return der
+def deriviraj(funk,xo,e,inti):
+    if inti==1:
+        der =  (value(f1,xo+e)-value(f1,xo-e))/(2*e)
+        return der
+    elif inti==0:
+        deri2 =((value(f1,xo))-value(f1,xo-e))/e
+        return deri2
 
 def derivacja2(funk,xd,xg,e):
+    a=[]
+    b=[]
     d=int((xg-xd)/e)
     for i in range(d):
         p=xd+i*e
@@ -38,60 +42,79 @@ def derivacja2(funk,xd,xg,e):
         b.append(k)
     return a,b
         
-print(deriviraj(f1,2,0.01))
-c,d = derivacja2(f1,0,10,0.1)
-g,h = ana_funk(fd,0,10,0.1)
-plt.figure(1)
-plt.scatter(c,d,label="num deri",color="r")
-plt.plot(g,h,label="ana deri",color="b")
-plt.show()
+def Graf(f1,xd,xg,e):
+
+    c,d = derivacja2(f1,xd,xg,e)
+    g,h = ana_funk(f1,xd,xg,e)
+    plt.figure(1)
+    plt.scatter(c,d,label="num deri",color="r")
+    plt.plot(g,h,label="ana deri",color="b")
+    plt.show()
 
 def integriraj(funk,xd,xg,e):
-    d=int((xg-xd)/e)
-    Id=0
-    Ig=0
-    Idd=[0]
-    Igg=[0]
-    pomakx=[xd]
-    for i in range(d):
-        Id=Id + (e)*value(funk,xd+i*e)
-        Idd.append(Id)
-        Ig=Ig +(e)*value(funk,(xd+e)+i*e)
-        Igg.append(Ig)
-        pomakx.append(xd+i*e)
-        
-    plt.figure(1)
-    plt.scatter(test,Idd,label="donja meda")
-    plt.scatter(test,Igg,label="donja meda")
-    plt.show()
-    
-    #neznam kao graf napravit neznam sto da stavim na osi da divergira s obe strane
+    broj_koraka=int((xg-xd)/e)
+    donja_suma=0
+    gornja_suma=0
+    donja_suma_list=[0]
+    gornja_suma_list=[0]
+    x_vrjednosti=[0]
+    for i in range(broj_koraka):
+        gornja_suma = gornja_suma + (e)*value(funk,(xd+e)+i*e)
+        gornja_suma_list.append(gornja_suma)
+        donja_suma=donja_suma +(e)*value(funk,xd+i*e)
+        donja_suma_list.append(donja_suma)
+        x_vrjednosti.append(xd+i*e)
+    return donja_suma,gornja_suma
 
-print(integriraj(f1,2,4,0.01))
-
-
+def integriraj2(funk,xd,xg,e):
+    broj_koraka=np.arange(xd,xg,e)
+    donja_suma=0
+    gornja_suma=0
+    donja_suma_list=[0]
+    gornja_suma_list=[0]
+    x_vrjednosti=[0]
+    X_vrjednosti=0
+    for i in range(len(broj_koraka)):
+        gornja_suma = gornja_suma + (e)*value(funk,(xd+e)+i*e)
+        gornja_suma_list.append(gornja_suma)
+        donja_suma=donja_suma +(e)*value(funk,xd+i*e)
+        donja_suma_list.append(donja_suma)
+        X_vrjednosti=xd+i*e
+        x_vrjednosti.append(X_vrjednosti)
+    return donja_suma,gornja_suma,x_vrjednosti
 
 def trap_integral(funk,xd,xg,e):
-    n=int((xg-xd)/e)
-    delx=(xg-xd)/n
-    
+    broj_koraka=np.arange(xd,xg,e)
+    delx=(xg-xd)/len(broj_koraka)
+    Id=0
     I=[0]
-    for i in range(n):
+    for i in range(len(broj_koraka)):
         Id=Id+delx/2*(value(funk,xd+i*e)+value(funk,xd+(i+1)*e))
         I.append(Id)
-    
-    return Id
+    return Id      
 
+def inte_graf(funk,xd,xg):
+    lista_epsilona=[0.1,0.5,1,2,3]
+    Gornja_G=[]
+    Donja_D=[]
+    X_Vr=[]
+    Trapez=[]
+    for i in range(len(lista_epsilona)):
+        j,k,g=integriraj2(funk,xd,xg,lista_epsilona[i])
+        ej=trap_integral(funk,xd,xg,lista_epsilona[i])
+        Trapez.append(ej)
+        Donja_D.append(j)
+        Gornja_G.append(k)
+        X_Vr.append(lista_epsilona[i])
+    plt.figure(1)
+    plt.scatter(lista_epsilona,Donja_D)
+    plt.scatter(lista_epsilona,Gornja_G)
+    plt.scatter(lista_epsilona,Trapez)
+    plt.show()
 
+#print(deriviraj(f1,2,0.001,0))
+#print(inte_graf(f1,2,4))
+#print(Graf(f1,0,10,0.01))
+#print(deriviraj(f1,2,0.01))
+#print(integriraj(f1,2,4,0.01))
 #print(trap_integral(f1,2,4,0.01))
-
-
-        
-
-
-        
-
-
-
-
-
