9539bcbc32d0e334df8f5d4d8ccdb234401810a8
Sandro095 <80029296+Sandro095@users.noreply.github.com>
2021-05-19 23:00:40 +0200

    Period dodan

--- Vjezbe5/harmonic_oscilator.py
+++ Vjezbe5/harmonic_oscilator.py
@@ -34,29 +34,25 @@ class harmonicoscilator:
         self.x=[]
         self.v=[]
         self.a=[]
+        self.T=[]
+        self.lista_vremena=[]
+        self.lista_brzina=[]
+        self.lista_polozaja=[]
+        self.lisa_akceleracija=[]
 
     def analy(self):
             self.neke_liste()
             for i in range(self.n):
-                self.time = self.time + self.dt
-                self.analy_polozaj = self.x0* m.sin(self.w*self.time)
-                self.analy_brzine = self.w * self.x0 *m.cos(self.w*self.time)
-                self.analy_akceleracija = (-1)*self.w*self.w * self.x0 *m.sin(self.w*self.time) 
+                self.analy_polozaj = self.x0* m.cos(self.w*self.time)
+                self.analy_brzine = -self.w * self.x0 *m.sin(self.w*self.time)
+                self.analy_akceleracija = (-1)*self.w*self.w * self.x0 *m.cos(self.w*self.time)
+                self.time = self.time + self.dt 
                 self.lisa_akceleracija.append(self.analy_akceleracija)
                 self.lista_brzina.append(self.analy_brzine)
                 self.lista_polozaja.append(self.analy_polozaj)
                 self.lista_vremena.append(self.time)
+            return self.lista_polozaja,self.lista_brzina,self.lisa_akceleracija,self.lista_vremena
 
-            plt.figure(1)
-            plt.scatter(self.lista_vremena,self.lista_polozaja,label="pomak")
-            plt.legend(loc="best")
-            plt.figure(2)
-            plt.scatter(self.lista_vremena,self.lista_brzina,label="brzina")
-            plt.legend(loc="best")
-            plt.figure(3)
-            plt.scatter(self.lista_vremena,self.lisa_akceleracija,label="akceleracija")
-            plt.legend(loc="best")
-            plt.show()
 
     def polozaj(self):
         self.neke_liste()
@@ -69,16 +65,59 @@ class harmonicoscilator:
             self.v.append(self.v0)
             self.a.append(self.ac)
             self.T.append(self.t)
-        
+
+        return self.x,self.v,self.a,self.T       
+
+
+    def graf_numerical(self):
+        a,b,c,d=self.polozaj()
         plt.figure(1)
-        plt.scatter(self.T,self.x,label="poz")
+        plt.plot(d,a,label="analitičko rješenje",color="blue")
         plt.legend(loc="best")
         plt.figure(2)
-        plt.scatter(self.T,self.v,label="brzina")
+        plt.plot(d,b,label="analitičko rješenje",color="blue")
         plt.legend(loc="best")
         plt.figure(3)
-        plt.scatter(self.T,self.a,label="akc")
+        plt.plot(d,c,label="analitičko rješenje",color="blue")
         plt.legend(loc="best")
         plt.show()
 
+
+    def graf_analy(self):
+        a,b,c,d=self.analy()
+        plt.figure(1)
+        plt.plot(d,a,label="analitičko rješenje",color="blue")
+        plt.legend(loc="best")
+        plt.figure(2)
+        plt.plot(d,b,label="analitičko rješenje",color="blue")
+        plt.legend(loc="best")
+        plt.figure(3)
+        plt.plot(d,c,label="analitičko rješenje",color="blue")
+        plt.legend(loc="best")
+        plt.show()
+
+
+    def graf_usporedba(self):
+        a,b,c,d=self.analy()
+        e,f,g,h=self.polozaj()
+        plt.figure(1)
+        plt.plot(d,a,label="analitičko rješenje",color="blue")
+        plt.plot(h,e,label="numeričko rješenje",color="red")
+        plt.legend(loc="best")
+        plt.show()
+
+    def period(self):
+        a,b,c,d= self.polozaj()
+        epsilon=0.001
         
+        for i in range(2,len(d),1):
+            if abs(a[0]-a[i])<=epsilon:
+                print("period je: " + str(d[i]) +" s")
+
+
+
+p1 = harmonicoscilator(20,5,10,0.01,10)
+#p1.graf_numerical()
+#p1.graf_analy()
+#p1.reset()
+p1.period()
